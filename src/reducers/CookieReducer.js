150609275f25ee77f20b9ff0def224d73b3639c8
alexandercortez <joelalexander10@outlook.com>
2020-01-14 23:39:58 -0600

    Connect redux

---
+++ src/reducers/CookieReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  cookies: {},
+  error: null,
+};
+
+const CookieReducer = (state = initialState, action) => {
+  return state;
+};
+
+export default CookieReducer;
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import CookieReducer from './CookieReducer';
+
+export default combineReducers({
+  CookieReducer,
+});
